bbed2c3833e92278d22fbe83f44e68a5e504cca8
HuJian <624598206@qq.com>
2016-12-29 16:27:34 +0800

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '@angular/material'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
+
+// AppComponent.annotations = [new Component({selector: 'app-root'})]
